e56b7621c4fbdf512e4f862517f59cd38095c286
NDylee34 <ylee34@nd.edu>
2025-03-16 21:56:19 -0400

    Update main.py

--- TidyData-Project/main.py
+++ TidyData-Project/main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Load the dataset
-file_path = "/mnt/data/olympics_08_medalists.csv"
-df = pd.read_csv(file_path)
+olympics_data = "olympics_08_medalists.csv"
+df = pd.read_csv(olympics_data)
 
 # Display basic information about the dataset
 df.info(), df.head()
